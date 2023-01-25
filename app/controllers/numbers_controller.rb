9ba8632775e6599cbc41dee7f9d3fea29a3970b9
syaoyaoyao <syao0@chicagobooth.edu>
2023-01-25 21:49:35 +0000

    finished in-class exercise

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -4,10 +4,21 @@ class NumbersController < ApplicationController
 
     5.times do
       giraffe = rand(1...100)
-      
+
       @zebra.push(giraffe)
     end
 
-    render({ :template => "lottery_stuff/woohoo.html.erb"})
-  end 
+    render({ :template => "lottery_stuff/woohoo.html.erb" })
+  end
+
+  def losers
+    @zebra = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+
+      @zebra.push(giraffe)
+    end
+    render({ :template => "lottery_stuff/no_woohoo.html.erb" })
+  end
 end
--- app/controllers/water_controller.rb
+++ app/controllers/water_controller.rb
@@ -1,16 +1,16 @@
-class WatersController < ApplicationController
+class WaterController < ApplicationController
   def crab
-    horoscope = "Confirmation of professional success could come your way, Cancer, and you're probably feeling excited and motivated to keep pushing. But you may find that increased responsibilities interfere with your social life. You wonder if friends have forgotten you. They haven't, but it will make you feel better if you squeeze in a few hours for your friends each week.
+    @horoscope = "Confirmation of professional success could come your way, Cancer, and you're probably feeling excited and motivated to keep pushing. But you may find that increased responsibilities interfere with your social life. You wonder if friends have forgotten you. They haven't, but it will make you feel better if you squeeze in a few hours for your friends each week.
     Remember what they say about all work and no play."
 
     @array_of_numbers = Array.new
 
     5.times do
       another_number = rand(1...100)
-      
+
       @array_of_numbers.push(another_number)
     end
-    
+
     render({ :template => "aqua_views/cancer.html.erb" })
   end
 
@@ -21,10 +21,10 @@ class WatersController < ApplicationController
 
     5.times do
       another_number = rand(1...100)
-      
+
       @array_of_numbers.push(another_number)
     end
-    
+
     render({ :template => "aqua_views/scorpio.html.erb" })
   end
 
@@ -36,10 +36,10 @@ class WatersController < ApplicationController
 
     5.times do
       another_number = rand(1...100)
-      
+
       @array_of_numbers.push(another_number)
     end
-    
+
     render({ :template => "aqua_views/pisces.html.erb" })
   end
 end
